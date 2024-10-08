b0fcb08e6adc37bfc4b97b235eb1ad4d636018ce
nathaliacbarroso <nathaliacbarroso.as@gmail.com>
2024-10-07 21:04:49 -0300

    Adicionei minha contribuição ao exercício em dupla

--- src/index.js
+++ src/index.js
@@ -10,3 +10,17 @@ const produtosDisponiveis = [
 
 // Carrinho de compras como um array de objetos
 let carrinho = [];
+console.log(carrinho)
+
+// Fazer tudo que eu escrevo no prompt entrar na let carrinho, deixar o array ocupado por um dos itens.
+
+// Função para adicionar produtos ao carrinho
+function adicionarProdutos() {
+let nomedoProduto = prompt("Qual dos produtos você deseja adicionar: Camisa, Calça, Sapato ou Boné?")
+// Utilizar um método to uppercase para verificar os itens
+// Após verificar, precisamos utilizar o método push para adicionar o que a pessoa escreveu no prompt na minha let carrinho
+
+alert(nomedoProduto)
+}
+adicionarProdutos()
+
