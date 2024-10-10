c362645879d8ff9c3ed6ba5e42a97cebec07c364
nathaliacbarroso <nathaliacbarroso.as@gmail.com>
2024-10-10 17:55:23 -0300

    Atualizações realizadas após a última aula.

--- src/index.js
+++ src/index.js
@@ -10,17 +10,26 @@ const produtosDisponiveis = [
 
 // Carrinho de compras como um array de objetos
 let carrinho = [];
-console.log(carrinho)
+console.log(carrinho);
 
 // Fazer tudo que eu escrevo no prompt entrar na let carrinho, deixar o array ocupado por um dos itens.
 
 // Função para adicionar produtos ao carrinho
 function adicionarProdutos() {
 let nomedoProduto = prompt("Qual dos produtos você deseja adicionar: Camisa, Calça, Sapato ou Boné?")
-// Utilizar um método to uppercase para verificar os itens
+// Julli falou para utilizar um método touppercase para verificar os itens
+const produtoEncontrado = produtosDisponiveis.find(produto => produto.nome.toUpperCase() === nomedoProduto);
+
 // Após verificar, precisamos utilizar o método push para adicionar o que a pessoa escreveu no prompt na minha let carrinho
+if (produtoEncontrado) {
+  carrinho.push(produtoEncontrado);
+  alert(`${produtoEncontrado.nome} foi adicionado ao carrinho.`);
+} else {
+  alert("Produto não encontrado. Tente novamente.");
+}
 
 alert(nomedoProduto)
+
 }
 adicionarProdutos()
 
